629d91e8b976b3ee555e875e8f6d5682bfa568f9
Egor <egor.urvancev123@gmail.com>
2023-01-16 17:51:06 +0500

    added Pagination and Filters

--- note/serializer.py
+++ note/serializer.py
@@ -4,15 +4,15 @@ from users.serializer import UsersModelSerializer
 
 
 class ProjectModelSerializer(ModelSerializer):
-    users = UsersModelSerializer(many=True)
+    # users = UsersModelSerializer(many=True)
     class Meta:
         model = Project
         fields = '__all__'
 
 
 class TodoModelSerializer(ModelSerializer):
-    user_create = UsersModelSerializer()
-    project = ProjectModelSerializer()
+    # user_create = UsersModelSerializer()
+    # project = ProjectModelSerializer()
     class Meta:
         model = Todo
         fields = '__all__'
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Users
 from .serializer import UsersModelSerializer
 
+
 class UsersModelViewSet(ModelViewSet):
     queryset = Users.objects.all()
     serializer_class = UsersModelSerializer
